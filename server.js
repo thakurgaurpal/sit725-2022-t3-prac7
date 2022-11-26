b7a2194518e047ecdb9733dd44ae5d0d563ed825
Gaurpal Singh Chauhan <chauhangaurpals@gmail.com>
2022-11-26 13:56:00 +1100

    updates week2

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express")
+var app = express()
+
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({extends: false}))
+
+const addNumbers=(number1, number2)=>{
+    var num1 =parseInt(number1)
+    var num2 =parseInt(number2)
+    var result= num1+num2;
+    return result;
+}
+
+
+app.get("/addTwoNumbers", (req, res)=>{
+    var number1=req.query.number1;
+    var number2=req.query.number2;
+    var result= addNumbers(number1, number2)
+    res.json({statuscode: 200,data: result, message: 'success'})
+})
+
+var port = process.env.port || 3000;
+app.listen(port,()=>{
+    console.log("App listening to: http://localhost:"+port)
+})
