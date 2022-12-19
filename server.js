a8a2441a7a27db6735334f5500f4143443e41259
Gaurpal Singh Chauhan <chauhangaurpals@gmail.com>
2022-12-19 17:09:46 +1100

    updated week 4

--- server.js
+++ server.js
@@ -5,19 +5,25 @@ app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const addNumbers = (number1, number2) => {
-    var num1 = parseInt(number1)
-    var num2 = parseInt(number2)
-    var result = num1 + num2;
-    return result;
-}
+const cardList = [
+    {
+        title: "pic2",
+        image: "images/pic2.png",
+        link: "About pic 2",
+        desciption: "Demo desciption about pic 2"
+    },
+    {
+        title: "pic 3",
+        image: "images/pic3.jpg",
+        link: "About pic 3",
+        desciption: "Demo desciption about pic 3"
+    }
+]
 
-app.get("/addTwoNumbers",(req,res) => {
-    var number1 = req.query.number1;
-    var number2 = req.query.number2;
-    var result = addNumbers(number1,number2)
-    res.json({statusCode: 200, data: result, message:"Success"})
+app.get('/api/projects',(req,res) => {
+    res.json({statusCode: 200, data: cardList, message:"Success"})
 })
+
 
 var port = process.env.port || 3000;
 
